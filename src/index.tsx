6893b499f8d3bcffd81c1db853fead54d8266591
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2020-12-11 15:59:56 +0100

    Add missing files

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,6 @@ import '@styles/styles.scss'
 import React, { FC } from 'react'
 import ReactDom from 'react-dom'
 
-// import { App } from '@components/app/app'
-const App: FC = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  )
-}
+import { App } from '@components/app/app'
 
 ReactDom.render(<App />, document.getElementById('root'))
