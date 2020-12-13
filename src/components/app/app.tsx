e4bffbc4328d92c494061fc9e7fb12ca6248e78d
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2020-12-13 16:15:33 +0100

    Add sidebar

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,18 @@
 import React, { Suspense, lazy } from 'react'
 
-const LazyStrawberryIcon = lazy(() => import('./strawberry'))
 export const App = (): React.ReactElement => (
-    <div>
-        <div>It works</div>
-        <h1 className="text-red-500">Hello TailWindCSS</h1>
-        <Suspense fallback={'loading...'}>
-            <LazyStrawberryIcon />
-        </Suspense>
+    <div className="w-screen h-screen bg-gray-200">
+        <div
+            className="transition-all
+            duration-500 
+            ease-in-out 
+            shadow 
+            w-24 
+            h-screen 
+            bg-gray-500 
+            hover:bg-gray-600 
+            hover:w-80"
+        >
+        </div>
     </div>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
     extend: {}
   },
   variants: {
-    extend: {}
+    extend: {
+      width: ['hover']
+    }
   },
   plugins: []
 }
