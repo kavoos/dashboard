a8046ebde69eb437fe36311d7a9147e59cc03217
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2020-12-13 21:34:03 +0100

    Add expand button to sidebar

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,18 +1,8 @@
-import React, { Suspense, lazy } from 'react'
+import React, { FC } from 'react'
+import { Sidebar } from '../sidebar/sidebar'
 
-export const App = (): React.ReactElement => (
-    <div className="w-screen h-screen bg-gray-200">
-        <div
-            className="transition-all
-            duration-500 
-            ease-in-out 
-            shadow 
-            w-24 
-            h-screen 
-            bg-gray-500 
-            hover:bg-gray-600 
-            hover:w-80"
-        >
-        </div>
-    </div>
+export const App: FC = (): React.ReactElement => (
+  <div className="w-screen h-screen bg-gray-200">
+    <Sidebar />
+  </div>
 )
---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,25 @@
+import React, { FC, useState } from 'react'
+
+export const Sidebar: FC = () => {
+  const [isOpen, setIsOpen] = useState(true)
+
+  return (
+    <div
+      className={`relative transition-all duration-500 ease-in-out shadow h-screen bg-gray-600 ${isOpen ? 'w-80' : 'w-16 '}`}
+    >
+      <button
+        className={`w-6 absolute bottom-4 left-4 fill-current text-white focus:outline-none`}
+        onClick={() => setIsOpen(!isOpen)}
+        >
+        <svg 
+          className={`transform duration-500 ease-in-out ${isOpen ? 'rotate-180' : 'rotate-0 '}`}
+          xmlns="http://www.w3.org/2000/svg" 
+          fill="none" 
+          viewBox="0 0 24 24" 
+          stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+        </svg>
+      </button>
+    </div>
+  )
+}
